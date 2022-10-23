0b1ae87e2c5e1d436de79b93856126bd7193139b
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-10-23 16:42:07 +0200

    metric

--- caped_ai_metrics/ScoreKeeper.py
+++ caped_ai_metrics/ScoreKeeper.py
@@ -215,7 +215,7 @@ class ClassificationScore:
                         tp  = tp + 1
                 else:
                         fp = fp + 1        
-            tp = tp / len(self.location_gt) * 100
+            tp = tp / len(self.location_pred) * 100
             fp = fp / len(self.location_pred) * 100
             fn = self._FalseNegatives()
             return tp, fn, fp, len(self.location_pred), len(self.location_gt)
