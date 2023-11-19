d43533dbb80f22543da7c9a71fe9f628d0ee8098
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-11-19 16:56:31 +0100

    metrics

--- caped_ai_metrics/ScoreKeeper.py
+++ caped_ai_metrics/ScoreKeeper.py
@@ -12,6 +12,7 @@ from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import  normalized_root_mse as mse
 import seaborn as sns
 from natsort import natsorted
+import os
 class SegmentationScore:
     """
     ground_truth: Input the directory contianing the ground truth label tif files
@@ -25,12 +26,12 @@ class SegmentationScore:
     taus: The list of thresholds for computing the metrics 
     
     """
-    def __init__(self, ground_truth, predictions, results_dir, pattern='*.tif', taus=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]):
-        self.ground_truth = natsorted([imread(file) for file in Path(ground_truth).glob(pattern)])
-        self.predictions = natsorted([imread(file) for file in Path(predictions).glob(pattern)])
+    def __init__(self, ground_truth_dir, predictions_dir, results_dir, pattern='*.tif', taus=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]):
+        self.ground_truth = natsorted([imread(os.path.join(ground_truth_dir, file)) for file in os.listdir(ground_truth_dir) if file.endswith(pattern)])
+        self.predictions = natsorted([imread(os.path.join(predictions_dir, file)) for file in os.listdir(predictions_dir) if file.endswith(pattern)])
         self.results_dir = results_dir
         self.taus = taus
-        
+            
     def seg_stats(self):
         
         stats_mse = []
